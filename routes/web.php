a291ef32d08aaca29dff96396335ae578917305f
shisir kafle <sishirdon@gmail.com>
2025-07-09 12:33:34 +0545

    adding logo and color grading for login page

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\FrontendController;
 // Landing page
 Route::get('/', [FrontendController::class, 'trending']);
 
+Route::get('/', [FrontendController::class, 'trending'])->name('trending');
+
+
 // Auth routes
 Route::get('/login', [FrontendController::class, 'login'])->name('login');
 // Route::get('/register', [FrontendController::class, 'register'])->name('register');
