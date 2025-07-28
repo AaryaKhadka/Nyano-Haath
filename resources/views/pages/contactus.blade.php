8b324f70bcc9219f46dc9892a1686511b9c3b466
sarojshah04 <sharanshah549@email.com>
2025-07-28 15:04:02 +0545

    adding webpage header and footer

--- resources/views/pages/contactus.blade.php
+++ resources/views/pages/contactus.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Us - Nyano Haath</title>
-    
-    <!-- Google Fonts for typography -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Caveat:wght@700&display=swap" rel="stylesheet">
-    
-    <!-- Font Awesome for Icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+@extends('layouts.app')
+
+@section('title', 'Login')
+
+  @section('styles')
+   
 
     <style>
         :root {
@@ -251,8 +243,10 @@
             }
         }
     </style>
-</head>
-<body>
+    @endsection
+
+
+@section('content')
 
     <div class="container">
         <header class="header">
@@ -362,6 +356,4 @@
             });
         });
     </script>
-
-</body>
-</html>
+@endsection
